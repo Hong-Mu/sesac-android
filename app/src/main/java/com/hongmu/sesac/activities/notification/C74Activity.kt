7b6eeeca27393dbdaa3e2af4d2202a94086a568e
Hong-Mu <hongmuchae@gmail.com>
2022-11-21 02:55:28 +0900

    C75: Notification 구성(C74)

--- app/src/main/java/com/hongmu/sesac/activities/notification/C74Activity.kt
+++ app/src/main/java/com/hongmu/sesac/activities/notification/C74Activity.kt
@@ -2,6 +2,9 @@ package com.hongmu.sesac.activities.notification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.media.AudioAttributes
 import android.media.RingtoneManager
@@ -56,6 +59,27 @@ class C74Activity : AppCompatActivity() {
                 setContentTitle("Title")
                 setContentText("Message")
             }
+
+            val actionIntent = Intent(this, C74SubActivity::class.java)
+            val actionPending = PendingIntent.getActivity(this, 20, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            builder.setContentIntent(actionPending)
+
+            val actionIntent2 = Intent(this, C74SubActivity::class.java)
+            val actionPending2 = PendingIntent.getActivity(this, 20, actionIntent2, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            // 버튼
+            builder.addAction(
+                NotificationCompat.Action.Builder(
+                    android.R.drawable.stat_notify_more,
+                    "Action",
+                    actionPending2
+                ).build()
+            )
+
+            val bigPicture = BitmapFactory.decodeResource(resources, R.drawable.sunset)
+            val style = NotificationCompat.BigPictureStyle()
+            style.bigPicture(bigPicture)
+            builder.setStyle(style)
 
             manager.notify(1, builder.build())
         }
---
+++ app/src/main/java/com/hongmu/sesac/activities/notification/C74SubActivity.kt
@@ -0,0 +1,12 @@
+package com.hongmu.sesac.activities.notification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hongmu.sesac.R
+
+class C74SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c74_sub)
+    }
+}
